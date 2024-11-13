e7140c474d302b6c475e8d9d2d8ea809f5254dae
Maier Ludentrop <agamemnonagross@gmail.com>
2024-11-13 22:38:42 +0300

    added PRICE checking function

--- indushka.py
+++ indushka.py
@@ -1,5 +1,9 @@
 from tradingview_ta import TA_Handler
+import requests
 from tabulate import tabulate
+
+from cfg import KEY
+
 
 assets = ['BTCUSDT', 'ETHUSDT', 'SOLUSDT', 'DOGEUSDT', 'PEPEUSDT']
 timeframes = ['1h', '4h', '1d']
@@ -32,6 +36,16 @@ def get_TA(asset, tf='1h', exchange='mexc'):
 
     recommendation = [[key, value] for key, value in recommendation.items()]
     return tabulate(recommendation, tablefmt="github")
+
+
+def get_asset(asset_id_base, asset_id_quote='USDT'):
+    url = f"https://rest.coinapi.io/v1/exchangerate/{asset_id_base.upper()}/{asset_id_quote.upper()}"
+    headers = {
+        "X-CoinAPI-Key": KEY
+    }
+
+    response = requests.get(url, headers=headers).json()['rate']
+    return round(response, 4)
 
 
 def make_table(data):
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from cfg import TOKEN
 
-from indushka import get_recs, get_TA, make_table
+from indushka import get_recs, get_TA, make_table, get_asset
 
 # All handlers should be attached to the Router (or Dispatcher)
 bot = Bot(token=TOKEN)
@@ -25,6 +25,11 @@ async def start(message: types.Message) -> None:
                         "По умолчанию биржа MEXC.")
 
 
+@dp.message_handler(commands=["price"])
+async def get_price(message: types.Message) -> None:
+    await message.answer(message.text)
+
+
 @dp.message_handler(commands=["sent"])
 async def start(message: types.Message) -> None:
     await message.answer('<pre>' + make_table(get_recs()) + '</pre>', parse_mode='html')
@@ -35,12 +40,15 @@ async def parse_and_reply(message: types.Message) -> None:
     request = message.text.split(' ')
     result = get_TA(*request)
 
-    if result is None:
-        await message.answer(f"<a href='https://www.tradingview.com/chart/?symbol={request[0].upper()}'>Биржа или "
-                             f"криптопара не найдена.</a>", parse_mode='html')
+    if not (result is None):
+        await message.answer('<pre>' + result + '</pre>', parse_mode='html')
+        return
+    elif isinstance(result, str):
+        await message.answer(get_asset(request[0]))
         return
     else:
-        await message.answer('<pre>' + result + '</pre>', parse_mode='html')
+        await message.answer(f"<a href='https://www.tradingview.com/chart/?symbol={request[0].upper()}'>Биржа или "
+                             f"криптопара не найдена.</a>", parse_mode='html')
 
 
 if __name__ == '__main__':
